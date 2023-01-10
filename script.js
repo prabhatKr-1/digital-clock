d6dc9cd8f523bf1de8d402a2d1bea9f6b48b3aaa
Prabhat Kumar <prabhatkumar.cse@gmail.com>
2023-01-10 19:44:11 +0530

    Added Seconds,AM-PM and a new bg image

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 hour = document.getElementById("hour");
 minutes = document.getElementById("min");
+seconds = document.getElementById("sec");
+ampm = document.getElementById("ampm");
 setInterval(function () {
   date = new Date();
   hrs = date.getHours();
   min = date.getMinutes();
-  hour.innerText = hrs % 12;
-  minutes.innerText = min;
-  // day = document.getEleme
-}, 400);
-ntById("date");
-// day.innerHTML = date.toString()
+  sec = date.getSeconds();
+  if (hrs % 12 < 10) {
+    hour.innerText = "0" + (hrs % 12);
+  } else {
+    hour.innerText = hrs % 12;
+  }
+  if (min < 10) {
+    minutes.innerText = "0" + min;
+  } else {
+    minutes.innerText = min;
+  }
+  if (sec < 10) {
+    seconds.innerText = "0" + sec;
+  } else {
+    seconds.innerText = sec;
+  }
+  if (hrs > 12) {
+    ampm.innerText = "PM";
+  } else {
+    ampm.innerText = "AM";
+  }
+}, 1000);
